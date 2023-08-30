72f87b95881f256835702e12f1b86c44c7e222e1
Pengpa0012 <peng.ilikha@gmail.com>
2023-08-31 06:28:51 +0800

    added controller detection

--- script/world.gd
+++ script/world.gd
@@ -1,11 +1,27 @@
 extends Node2D
 
+const PLAYER_SCENE_PATH = preload("res://scenes/player.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var players = []
 
+#func _ready():
+#	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
+#
+#
+#func _on_joy_connection_changed(device_id, connected):
+#	if connected:
+#		print(Input.get_joy_name(device_id))
+#	else:
+#		print("Keyboard")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	var connected_joypads = Input.get_connected_joypads()
+	print(connected_joypads)
+#	if connected_joypads.size() > 0:
+#		var player_instance = PLAYER_SCENE_PATH.instantiate()
+#		players.append(player_instance)
+#		add_child(player_instance)
+#
+#		for i in range(players.size()):
+#			players[i].position.x = i * 100 
+	
