099502b0b25a1c435c3e8d871101d5499f6fdd22
Pengpa0012 <peng.ilikha@gmail.com>
2023-08-31 10:26:18 +0800

    add player when new controller detected

--- script/world.gd
+++ script/world.gd
@@ -4,24 +4,18 @@ const PLAYER_SCENE_PATH = preload("res://scenes/player.tscn")
 
 var players = []
 
-#func _ready():
-#	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
-#
-#
-#func _on_joy_connection_changed(device_id, connected):
-#	if connected:
-#		print(Input.get_joy_name(device_id))
-#	else:
-#		print("Keyboard")
 
 func _process(_delta):
 	var connected_joypads = Input.get_connected_joypads()
-	print(connected_joypads)
-#	if connected_joypads.size() > 0:
-#		var player_instance = PLAYER_SCENE_PATH.instantiate()
-#		players.append(player_instance)
-#		add_child(player_instance)
-#
-#		for i in range(players.size()):
-#			players[i].position.x = i * 100 
+	for player in connected_joypads:
+		if not players.has(player):
+			add_player(player)
 	
+func add_player(index):
+	players.append(index)
+	var player_instance = PLAYER_SCENE_PATH.instantiate()
+	player_instance.position.x = 100
+	add_child(player_instance)
+
+func remove_player(index):
+	players.erase(index)
