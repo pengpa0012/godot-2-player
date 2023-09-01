f9952ac091d354cc990c1b88cc1db7d510a576f3
Pengpa0012 <peng.ilikha@gmail.com>
2023-09-01 08:44:50 +0800

    change player input

--- script/world.gd
+++ script/world.gd
@@ -4,18 +4,19 @@ const PLAYER_SCENE_PATH = preload("res://scenes/player.tscn")
 
 var players = []
 
-
-func _process(_delta):
-	var connected_joypads = Input.get_connected_joypads()
-	for player in connected_joypads:
-		if not players.has(player):
-			add_player(player)
-	
-func add_player(index):
-	players.append(index)
-	var player_instance = PLAYER_SCENE_PATH.instantiate()
-	player_instance.position.x = 100
-	add_child(player_instance)
-
-func remove_player(index):
-	players.erase(index)
+#
+#func _process(_delta):
+#	var connected_joypads = Input.get_connected_joypads()
+#	for player in connected_joypads:
+#		if len(players) != 5:
+##			print(player)
+#			add_player(player)
+#
+#func add_player(index):
+#	players.append(index)
+#	var player_instance = PLAYER_SCENE_PATH.instantiate()
+#	player_instance.position.x = randi_range(0, 100)
+#	add_child(player_instance)
+#
+#func remove_player(index):
+#	players.erase(index)
