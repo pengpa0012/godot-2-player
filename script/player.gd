eb8cad825be1bc43844173824fdfde4c0533f154
Pengpa0012 <peng.ilikha@gmail.com>
2023-09-02 18:16:51 +0800

    update adding player

--- script/player.gd
+++ script/player.gd
@@ -4,18 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@export var move_left := "ui_left"
-@export var move_right := "ui_right"
-@export var jump := "ui_accept"
-@export var player_index := 0
-
+@export var player_index := 0 
+  
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	print(InputEvent)
+	
 	if Input.is_joy_button_pressed(player_index, 0) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
--- script/world.gd
+++ script/world.gd
@@ -4,19 +4,18 @@ const PLAYER_SCENE_PATH = preload("res://scenes/player.tscn")
 
 var players = []
 
-#
-#func _process(_delta):
-#	var connected_joypads = Input.get_connected_joypads()
-#	for player in connected_joypads:
-#		if len(players) != 5:
-##			print(player)
-#			add_player(player)
-#
-#func add_player(index):
-#	players.append(index)
-#	var player_instance = PLAYER_SCENE_PATH.instantiate()
-#	player_instance.position.x = randi_range(0, 100)
-#	add_child(player_instance)
-#
-#func remove_player(index):
-#	players.erase(index)
+
+func _process(_delta):
+	var connected_joypads = Input.get_connected_joypads()
+	for player in connected_joypads:
+		if player not in players:
+			add_player(player)
+
+func add_player(index):
+	players.append(index)
+	var player_instance = PLAYER_SCENE_PATH.instantiate()
+	player_instance.position.x = randi_range(0, 100)
+	add_child(player_instance)
+
+func remove_player(index):
+	players.erase(index)
