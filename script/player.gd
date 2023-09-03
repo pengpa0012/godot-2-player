4096a6a44d9396e59ef2bcb859ccf77c01f5eff4
Pengpa0012 <peng.ilikha@gmail.com>
2023-09-03 17:29:31 +0800

    added death animation

--- script/player.gd
+++ script/player.gd
@@ -20,9 +20,6 @@ var player_data = {
   
 func _physics_process(delta):
 	
-	if player_data["health"] <= 0:
-		queue_free()
-	
 	if not is_on_floor():
 		velocity.y += player_data["gravity"] * delta
 	
@@ -32,25 +29,28 @@ func _physics_process(delta):
 	var directionX = Input.get_joy_axis(player_index, 0)
 	var directionY = Input.get_joy_axis(player_index, 1)
 	
-	if directionX:
-		if is_on_floor():
-			$AnimationPlayer.play("run")		
-		if directionX > 0:
-			$Sprite2D.flip_h = false
-			$Marker2D.position.x = 25
-			player_data["faceRight"] = true
-		else:
-			$Sprite2D.flip_h = true
-			$Marker2D.position.x = -25
-			player_data["faceRight"] = false
-		velocity.x = directionX * SPEED
+	if player_data["health"] <= 0:
+		$AnimationPlayer.play("death")
 	else:
-		if is_on_floor():
-#			if directionY:
-#				$AnimationPlayer.play("crouch")
-#			else:
-			$AnimationPlayer.play("idle")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if directionX:
+			if is_on_floor():
+				$AnimationPlayer.play("run")		
+			if directionX > 0:
+				$Sprite2D.flip_h = false
+				$Marker2D.position.x = 25
+				player_data["faceRight"] = true
+			else:
+				$Sprite2D.flip_h = true
+				$Marker2D.position.x = -25
+				player_data["faceRight"] = false
+			velocity.x = directionX * SPEED
+		else:
+			if is_on_floor():
+	#			if directionY:
+	#				$AnimationPlayer.play("crouch")
+	#			else:
+				$AnimationPlayer.play("idle")
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	if not is_on_floor():
 		$AnimationPlayer.play("jump")
@@ -95,6 +95,6 @@ func shoot():
 func player_hurt(amount):
 	player_data["health"] -= amount
 	
-#func _on_animation_player_animation_finished(anim_name):
-#	if anim_name == "crouch":
-#		$AnimationPlayer.stop()
+func _on_animation_player_animation_finished(anim_name):
+	if "death" in anim_name:
+		queue_free()
