c07cbf2d2d42bd3b3bf09552368281b6170e1634
Pengpa0012 <peng.ilikha@gmail.com>
2023-09-11 07:54:41 +0800

    added 2 map

--- script/select_map.gd
+++ script/select_map.gd
@@ -2,5 +2,12 @@ extends Control
 
 
 func _on_button_3_button_down():
-	print("pressing")
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+
+func _on_button_2_button_down():
+		get_tree().change_scene_to_file("res://scenes/world_3.tscn")
+
+
+func _on_button_button_down():
+	get_tree().change_scene_to_file("res://scenes/world_2.tscn")
