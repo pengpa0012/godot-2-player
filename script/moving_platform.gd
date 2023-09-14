e6883ec4c4e1c029608fe717a65853cee648a084
Pengpa0012 <peng.ilikha@gmail.com>
2023-09-14 08:51:47 +0800

    update world 2 tilemap

--- script/moving_platform.gd
+++ script/moving_platform.gd
@@ -1,10 +1,10 @@
 extends StaticBody2D
 
 @export var MOVE_OFFSET = 100
+@export var movingRight = true
 @export var SPEED = 40
 
 var initialPos = 0
-var movingRight = true
 
 func _ready():
 	initialPos = self.position.x
