89773e4ed04964389d98efae61124f3e3759dadc
Pengpa0012 <peng.ilikha@gmail.com>
2023-09-20 06:57:35 +0800

    added countdown UI, custom font

--- script/world.gd
+++ script/world.gd
@@ -4,12 +4,15 @@ const PLAYER_SCENE_PATH = preload("res://scenes/player.tscn")
 @onready var display_size = get_viewport().get_visible_rect().size
 var colors = ["black", "blue"]
 @onready var GLOBAL = get_node("/root/Global")
+@onready var counter = $Countdown/Label
 var drop_players = false
 
 func _process(_delta):
 	var connected_joypads = Input.get_connected_joypads()
-#	if len(GLOBAL.players) <= 0:
-#		$RestartTimer.start()
+	counter.text = "[center]%s[/center]" % str(round($CountDownTimer.time_left))
+	
+	if $CountDownTimer.time_left <= 0:
+		$Countdown.visible = false
 
 	if drop_players:
 		for player in connected_joypads:
@@ -26,9 +29,6 @@ func add_player(index):
 	player_instance.player_color = colors[randomize]
 	add_child(player_instance)
 
-
-#func _on_restart_timer_timeout():
-#	print(len(GLOBAL.players))	
 
 func _on_count_down_timer_timeout():
 	drop_players = true
