54e92ca20f31e3e24d1f0797a09368efa4901353
Pengpa0012 <peng.ilikha@gmail.com>
2023-09-21 20:00:20 +0800

    update instruction scene, result scene

--- script/world.gd
+++ script/world.gd
@@ -10,7 +10,8 @@ func _process(_delta):
 	counter.text = "[center]%s[/center]" % str(round($CountDownTimer.time_left))
 	if len(GLOBAL.players) <= 1 and !$Countdown.visible:
 		GLOBAL.players = []
-		get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
+		$Winner.visible = true
+#		get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
 		
 	if $CountDownTimer.time_left <= 0:
 		$Countdown.visible = false
@@ -28,6 +29,12 @@ func add_player(index):
 
 func _on_count_down_timer_timeout():
 	var connected_joypads = Input.get_connected_joypads()	
+	print(connected_joypads)
 	for player in connected_joypads:
 		if player not in GLOBAL.players:
 			add_player(player)
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
+	$Winner.visible = false
